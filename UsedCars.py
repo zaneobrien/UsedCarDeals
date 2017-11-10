0b1063ed3b0e8572b01d4ef36bdd16c4e6e1bde6
Zane OBrien <Zane@Zanes-MacBook-Air.local>
2017-11-09 17:59:00 -0800

    adding Edmunds API and Craigslist API functionality

--- UsedCars.py
+++ UsedCars.py
@@ -4,54 +4,60 @@ import json
 
 
 class UsedCar(object):
-
-#make_call('/v1/api/vehiclephoto/service/findphotosbystyleid', comparator='simple', styleId='3883')
-#https://api.edmunds.com/v1/api/tmv/tmvservice/calculateusedtmv?styleid=100001210&condition=Outstanding&mileage=25000&zip=90069&fmt=json&api_key=8tx5sraszgugftzfu2rtdpbw
     def Edmondresponse(self):
-        """put json into a dictionary"""
-
-        #get styleID by the make model and year of car
+        """Makes an API call to Edmunds and puts response from json into a dictionary"""
+        # get styleID by the make model and year of car
         api = Edmunds('8tx5sraszgugftzfu2rtdpbw') # use Edmunds('YOUR API KEY', True) for debug mode
-        styleidResponse = api.make_call('/api/vehicle/v2/ford/f150/2011/styles')
-        styleIDJson = json.dumps(styleidResponse)
+        styleIDResponse = api.make_call('/api/vehicle/v2/'+self.make+'/'+self.model+'/'+self.year+'/styles')
+        styleIDJson = json.dumps(styleIDResponse)
         styleIDDict = json.loads(styleIDJson)
-        #extract the correct styleID for the correcponding make,model,year
-        #for key in styleIDDict.items():
-        #    if key = self.
-        #    print key
+        # extract the correct styleID for the corresponding make,model,year
+        for i in styleIDDict['styles']:
+            if self.trim == i['trim']:
+                print ('Assigning ID ' + str(i['id']) + ' to the vehicle')
+                #assignes the first ID that it finds in the json...not ideal
+                self.id = i['id']
+                break
+
+        response = api.make_call('/v1/api/tmv/tmvservice/calculateusedtmv', styleId=self.id,condition=self.condition,mileage=self.mileage, zip=self.zipcode)
+        carPriceJson = json.dumps(response)
+        carPriceDict = json.loads(carPriceJson)
+        self.usedprice = carPriceDict["tmv"]["totalWithOptions"]["usedPrivateParty"]
+        print ('Used Price for '+ self.year +' '+ self.make +' '+ self.model + ' '+ self.trim)
+        print (carPriceDict["tmv"]["totalWithOptions"]["usedPrivateParty"])
 
 
+    def CraigslistCompare(self):
+        """Uses CraigsList scraper to search deals in the area and gets price"""
+        CraigslistCar = CraigslistForSale(category='cta', site=self.city,filters={'query':self.year +' '+ self.make +' '+ self.model +' '+ self.trim})
+        print 'Searching Craigslist:' + self.year +' '+ self.make +' '+ self.model + ' '+ self.trim
 
-        #print styleIDDict["styles"][0]
-        #self.styleid = styleIDDict[]
-        #print styleIDJson
-        #self.styleID
-        #print styleidResponse
-
-
-        response = api.make_call('/v1/api/tmv/tmvservice/calculateusedtmv', styleId='101353967',condition=self.condition,mileage=self.mileage, zip=self.zipcode)
-        carJson = json.dumps(response)
-        carDict = json.loads(carJson)
-        self.usedprice = carDict["tmv"]["totalWithOptions"]["usedPrivateParty"]
-        print ('-----Used Price-----')
-        print (carDict["tmv"]["totalWithOptions"]["usedPrivateParty"])
-
-    def Craigslist(self):
-        """uses CraigsList scraper to see if the car is availible"""
-        #print CraigslistForSale.show_filters(category='cta')
-
-
-    #def ComparePrice(self):
+        for result in CraigslistCar.get_results(limit=10):
+            #take off '$'
+            dollarPrice = result["price"]
+            actualprice = float(dollarPrice[1:])
+            if actualprice < self.usedprice:
+                print ('Good deal: ' + result["name"] +'    '+ result["price"]+ '   ' + result['url'])
+            elif actualprice < (.9*self.usedprice):
+                print ('Great deal: ' + result["name"] +'    '+ result["price"]+ '   ' + result['url'])
+            elif actualprice < (.8*self.usedprice):
+                print ('Fantastic deal: ' + result["name"] +'    '+ result["price"]+ '   ' + result['url'])
+            elif actualprice < (.7*self.usedprice):
+                print ('This is a Steal! : ' + result["name"] +'    '+ result["price"]+ '   ' + result['url'])
+            else:
+                print ('Bad deal! : ' + result["name"] +'    '+ result["price"]+ '   ' + result['url'])
 
 
-    def __init__(self,make,model,year,mileage,condition,zipcode):
+    def __init__(self,make,model,year,trim,mileage,condition,city,zipcode):
         self.make = make
         self.model = model
         self.year = year
+        self.trim = trim
         self.mileage = mileage
         self.condition = condition
+        self.city = city
         self.zipcode = zipcode
 
-Car = UsedCar('ford','f150','2011','150000','Outstanding','92024')
+Car = UsedCar('ford','f150','2011','XLT','100000','Outstanding','sandiego','92024')
 Car.Edmondresponse()
-Car.Craigslist()
+Car.CraigslistCompare()
